d7b07cbb8a6def5a93f4b577997700c84a04ff8e
Eduardo Esplinio <63748745+eduesplinio@users.noreply.github.com>
2024-10-20 15:24:02 +0000

    finaliza endpoint `/api/v1/status`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,13 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toBe(parsedUpdatedAt);
+  
+  expect(responseBody.depedencies.database.version).toEqual("16.0");
+  expect(responseBody.depedencies.database.max_connections).toEqual(100);
+  expect(responseBody.depedencies.database.opened_connections).toEqual(1);
 });
