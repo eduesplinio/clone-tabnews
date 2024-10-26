04d96d8e5a39de1bb587d111c050cd43cdfdc864
Eduardo Esplinio <63748745+eduesplinio@users.noreply.github.com>
2024-10-26 12:40:01 +0000

    faz `databse.js` client.connect() mais robusto a erros

--- infra/database.js
+++ infra/database.js
@@ -8,14 +8,22 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
-    try {
+  console.log('Credenciais do Postgres:', {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  })
+  try {
+        await client.connect();
         const result = await client.query(queryObject);
         await client.end();
         return result;
 
     } catch (error) {
       console.error(error);
+      throw error;
     } finally {
         await client.end();
     }
