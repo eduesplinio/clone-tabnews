1b1c8ceb5a7a118c4dd76de45390d6115afc6950
Eduardo Esplinio <63748745+eduesplinio@users.noreply.github.com>
2025-02-26 01:07:47 +0000

    adds `wait-for-postgres.js` script

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,25 @@
+const { exec } = require("node:child_process");
+let count = 0;
+const spinner = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
+
+function checkPostgres() {
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
+
+  function handleReturn(err, stdout) {
+    if (!stdout.includes("accepting connections")) {
+      const frameIndex = count % spinner.length;
+
+      process.stdout.write(
+        `\r🟡 Aguardando Postgres aceitar conexões ${spinner[frameIndex]} `,
+      );
+
+      count++;
+      setTimeout(checkPostgres, 250);
+      return;
+    }
+
+    process.stdout.write("\r🟢 Postgres está pronto e aceitando conexões!\n");
+  }
+}
+
+checkPostgres();
