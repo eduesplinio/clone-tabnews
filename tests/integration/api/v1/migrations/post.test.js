5589100075d2870f4444b1c50d3e945c3833381d
Eduardo Esplinio <63748745+eduesplinio@users.noreply.github.com>
2025-07-14 23:09:10 +0000

    fix `prettier` style linting

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -8,7 +8,7 @@ beforeAll(async () => {
 
 test("POST to /api/v1/migrations should return 200", async () => {
   const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: 'POST',
+    method: "POST",
   });
   expect(response1.status).toBe(201);
 
@@ -18,7 +18,7 @@ test("POST to /api/v1/migrations should return 200", async () => {
   expect(response1Body.length).toBeGreaterThan(0);
 
   const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: 'POST',
+    method: "POST",
   });
   expect(response2.status).toBe(200);
 
