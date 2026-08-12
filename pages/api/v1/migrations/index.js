b71dff0a4449aea3a9fde9c6ad13647b2069a71b
Eduardo Esplinio <63748745+eduesplinio@users.noreply.github.com>
2026-08-12 10:38:19 +0000

    style: remove space in `resolve`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -17,7 +17,7 @@ export default async function migrations(request, response) {
     const defaultMigrationOptions = {
       dbClient: dbClient,
       dryRun: true,
-      dir: resolve ("infra", "migrations"),
+      dir: resolve("infra", "migrations"),
       direction: "up",
       verbose: true,
       migrationsTable: "pgmigrations",
